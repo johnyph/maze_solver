9a37804222d203cdac449f15da10d2cea2d3fed7
Ioannis-Andreas Philippas <ioannis.philippas@gmail.com>
2016-10-05 19:55:10 +0300

    Change outpout functions

--- lib/maze_solver.rb
+++ lib/maze_solver.rb
@@ -2,36 +2,51 @@ require "maze_solver/version"
 require "maze_solver/position"
 require "maze_solver/element"
 require "maze_solver/maze"
+require "json"
 
 module MazeSolver
 
-  def solve(file_name)
+  def to_json(file_name)
     
     maze_array = from_file(file_name)
     maze_list = to_maze(maze_array)
     maze = Maze.new(maze_list)
-   
-    to_image(maze_list, maze.solve, maze_array)
-  end
-  
-  def from_file(file_name)
-    ## Read file and export it to a 2D array
-    ## Throw error if file doesnt exist
-    begin
-      File.readlines(file_name).map { |line| line.split('') }
-    rescue abort("can't open #{file_name}" )
+    solution = maze.solve
+
+    solution_array = solution.map do |element|
+      { value: element.value, position: "(#{element.position.x}, #{element.position.y})" }
     end
+
+    JSON.generate(solution_array)
   end
 
-  def to_image(maze, shortest_path, maze_array)
-   
-    maze.each do |element|
-      maze_array[element.position.y][element.position.x] = '*' if shortest_path.find { |se| se == element }
+  def to_image(file_name)
+    ## Print solution to console
+    maze_array = from_file(file_name)
+    maze_list = to_maze(maze_array)
+    maze = Maze.new(maze_list)
+    solution = maze.solve
+
+    maze_list.each do |element|
+      maze_array[element.position.y][element.position.x] = '*' if solution.find { |se| se == element }
     end
 
     puts '!!!!!!!SOLUTIOM!!!!!!!'
     print maze_array.join('')
-    puts '^^^^^^^^^^^^^^^^^^^^^^'
+
+    nil
+  end
+
+  private
+
+  def from_file(file_name)
+    ## Read file and export it to a 2D array
+    ## Throw error if file doesnt exist
+
+    begin
+      File.readlines(file_name).map { |line| line.split('') }
+    rescue abort("can't open #{file_name}" )
+    end
   end
 
   def to_maze(maze_array)
@@ -53,6 +68,6 @@ module MazeSolver
     maze
   end
 
-  module_function :solve, :from_file, :to_maze, :to_image
+  module_function :to_json, :from_file, :to_maze, :to_image
 end
 
